3b429de8977445ba190462cd69b4adfbcaa34bfa
vilkan32 <velislv@mail.bg>
2020-08-08 14:09:17 +0300

    inititalCommit

---
+++ HttpTrigger1/__init__.py
@@ -0,0 +1,34 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    method = req.method
+
+    if method.lower() == 'post':
+        logging.info('Post Method Inside')
+    elif method.lower() == 'get':
+        logging.info('Get Method Inside')
+
+    name = req.params.get('name')
+
+    
+
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello {name}!")
+    else:
+        return func.HttpResponse(
+             "Please pass a name on the query string or in the request body",
+             status_code=400
+        )
